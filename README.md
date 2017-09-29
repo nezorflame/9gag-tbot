bf9c8728188115731435827b34b5497bd9ef9f60
Ilya Danilkin <nezorflame@gmail.com>
2017-09-29 17:13:45 +0300

    rewrote to the API library

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 Telegram bot for browsing 9GAG posts, written in pure Go.
 
-Right now the code just allows you to login and get top posts from Hot.
+Based on the unofficial API which is still being worked on [here](https://github.com/nezorflame/ninegago).
 
-This wouldn't be possible without the great work from [@and3rson](https://github.com/and3rson). Please check out his API example written on Python [here](https://github.com/and3rson/nineapi).
+Right now the code just allows you to login and get top posts from Hot.
--- main.go
+++ main.go
@@ -1,95 +1,47 @@
 package main
 
 import (
-	"encoding/json"
+	"flag"
 	"fmt"
-	"html"
 	"log"
-	"strconv"
-)
 
-// API URLs
-const (
-	AdURL         = "http://ad.9gag.com"
-	AdminURL      = "http://admin.9gag.com"
-	APIURL        = "http://api.9gag.com"
-	CommentURL    = "http://comment.9gag.com"
-	CommentCDNURL = "http://comment-cdn.9gag.com"
-	NotifyURL     = "http://notify.9gag.com"
-)
-
-// API endpoint paths
-const (
-	LoginPath    = "/v2/user-token/"
-	PostListPath = "/v2/post-list/"
-)
-
-// App consts
-const (
-	appID      = "com.ninegag.android.app"
-	deviceType = "android"
-	bucketName = "MAIN_RELEASE"
-	commentCDN = "a_dd8f2b7d304a10edaf6f29517ea0ca4100a43d1b"
+	"github.com/nezorflame/ninegago"
 )
 
 var (
-	appToken, deviceUUID string
+	username, password string
 
-	userData *UserData
+	apiClient *ninegago.APIClient
 )
 
 func init() {
-	appToken = randomSHA1HexStr()
-	deviceUUID = randomUUIDHexStr()
+	flag.StringVar(&username, "u", "", "9GAG username")
+	flag.StringVar(&password, "p", "", "9GAG password")
+	flag.Parse()
+
+	if username == "" || password == "" {
+		username, password = flag.Arg(0), flag.Arg(1)
+	}
+
+	if username == "" || password == "" {
+		flag.Usage()
+		log.Fatal("Wrong credentials")
+	}
 }
 
 func main() {
-	login("someusername", "somepassword")
-	fmt.Printf("Welcome, %s\n", userData.FullName)
-	getHot(10)
-}
-
-func login(username, password string) {
-	args := map[string]string{
-		"loginMethod": "9gag",
-		"loginName":   username,
-		"password":    getMD5HexStr(password),
-		"pushToken":   randomSHA1HexStr(),
-		"language":    "en_US",
-	}
-	respBody, err := requestGET(APIURL+LoginPath+urlArgsStr(args), true)
-	if err != nil {
+	apiClient = ninegago.NewAPIClient()
+	if err := apiClient.Login(username, password); err != nil {
 		log.Fatal(err)
 	}
 
-	resp := LoginResponse{}
-	if err = json.Unmarshal(respBody, &resp); err != nil {
+	fmt.Printf("Welcome, %s\n", apiClient.User.FullName)
+
+	if posts, err := apiClient.GetHotPosts("hot", 10); err == nil {
+		for _, p := range posts {
+			fmt.Println(p.Title, p.URL, p.Images.Image700.URL)
+		}
+	} else {
 		log.Fatal(err)
-	}
-
-	appToken = resp.Data.UserToken
-	userData = &resp.Data.User
-}
-
-func getHot(count int) {
-	args := map[string]string{
-		"group":      "1",
-		"type":       "hot",
-		"itemCount":  strconv.Itoa(count),
-		"entryTypes": "animated,photo,video,album",
-		"offset":     "10",
-	}
-	respBody, err := requestGET(APIURL+PostListPath+urlArgsStr(args), true)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	resp := PostListResponse{}
-	if err = json.Unmarshal(respBody, &resp); err != nil {
-		log.Fatal(err)
-	}
-
-	for _, p := range resp.Data.Posts {
-		fmt.Println(html.UnescapeString(p.Title), p.URL, p.Images.Image700.URL)
 	}
 }
